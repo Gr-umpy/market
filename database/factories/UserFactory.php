471fc59c6cb9c4b2bb1511877e5af83b8a90929a
lisandru <lisandrusecondi@gmail.com>
2026-02-04 16:43:34 +0100

    les utilisateurs ont un prenom, nom de famille et sont admins ou non

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,7 +24,8 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+            'first_name' => fake()->firstName(),
+            'last_name' => fake()->lastName(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
@@ -39,6 +40,12 @@ class UserFactory extends Factory
     {
         return $this->state(fn (array $attributes) => [
             'email_verified_at' => null,
+        ]);
+    }
+    public function is_admin(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'is_admin' => 1,
         ]);
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,9 +17,11 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        User::factory()->is_admin()->create([
+            'first_name' => 'Lisandru',
+            'last_name' => 'Secondi',
+            'email' => 'lisandrusecondi@gmail.com',
+            'password' => 'test'
         ]);
     }
 }
